667ab6151ccee936f22c6b5f5a228fef498db314
Fatih Arslan <ftharsln@gmail.com>
2011-08-15 23:48:49 +0300

    Add white spaces for more readability Change variable names. Wtf is x,y,z ??

--- sinerji/createsynergyconf.py
+++ sinerji/createsynergyconf.py
@@ -4,16 +4,19 @@
 import os
 from socket import gethostname
 subnames = []
+
 ### Takes a tuple of names and write the screen section of synergy.conf
 def screens(*screen):
     synergyconf = open(os.path.join(os.path.expanduser("~"), ".synergy.conf"), 'w')
     synergyconf.write('section: screens\n')
-    for x in screen[0]:
-        if x is not None:    ## If the comboBox is not empty
-            domains = x.split("_")
-            synergyconf.write("      %s:\n" % domains[2])
+
+    for hostnames in screen[0]:
+        if hostnames is not None:    ## If the comboBox is not empty
+            host = hostnames.split("_")
+            synergyconf.write("\t%s:\n" % host[2])
         else:
             pass
+
     synergyconf.write('end\n\n')
     synergyconf.close()
 
@@ -21,18 +24,20 @@ def screens(*screen):
 def links(*screen):
     synergyconf = open(os.path.join(os.path.expanduser("~"), ".synergy.conf"), 'a')
     synergyconf.write('section: links\n')
-    for y in screen[0]:
-        if y is not None: ## If the comboBox is not empty
-            subnames.append(y.split('_'))
+
+    for hostnames in screen[0]:
+        if hostnames is not None: ## If the comboBox is not empty
+            subnames.append(hostnames.split('_'))
         else:
             pass
+
     ### writing the host part ####
     synergyconf.write("\t%s:\n" % gethostname())
-    for z in subnames:
-            if z[0] == 'host':
-                pass
-            else:
-                synergyconf.write("\t%s = %s\n" % (z[0], z[2]))
+    for host in subnames:
+        if host[0] == 'host':
+            pass
+        else:
+            synergyconf.write("\t\t%s = %s\n" % (host[0], host[2]))
 
     ### writing the client part ###
     for clients in subnames:
